bbbdca9de7c8f1019bfd54141d311403db0446a8
David <david2020martin@gmail.com>
2024-08-09 13:09:03 -0400

    added form component

---
+++ components/form.tsx
@@ -0,0 +1,22 @@
+import { Image, StyleSheet, Platform } from 'react-native';
+import {Text, View} from 'react-native';//import { Text} from 'react-native-reanimated/lib/typescript/Animated';
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import '../app/global.css'
+import { green } from 'react-native-reanimated/lib/typescript/reanimated2/Colors';
+
+
+const Form = () =>{
+
+    return(
+        <>
+
+        </>
+    )
+}
+
+export default Form
