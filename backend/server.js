8fb4aa0702266285973ce37dfb8ca08f0bcf4aaa
David <david2020martin@gmail.com>
2024-08-21 16:24:27 -0400

    testing more routes

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,24 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
-port = 3000;
+const port = 3000;
 const  app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 
+app.get('/test1',(req,res)=>{
+
+    res.send("I am the server talking to you ")
+})
+
 app.use('/',(req,res)=>{
+    console.log("recived request")
     res.send("hello World");
-    
+
 })
 //Test post Function
-app.post('/',(req,res)=>{
+app.post('/test',(req,res)=>{
     const {username,password} = req.body;
     console.log("recieved data", {username,password})
 
